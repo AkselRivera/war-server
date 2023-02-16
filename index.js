90faae16e6ccc39188aa5e872f38fa4ec5edc911
Aksel <moralesaksel2@gmail.com>
2023-02-15 18:57:59 -0600

    Request ip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const requestIp = require("request-ip");
 const app = express();
 const port = 8080;
 
@@ -9,17 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  visit.push(req.ip);
+  visit.push(requestIp.getClientIp(req));
   res.send("Hello CX Devs you found me!");
 });
 
 app.post("/result", (req, res) => {
   storage.push({ user: req.ip?.user, pass: req.body?.pass });
+  console.log(storage);
   res.send("What is this endpoint doing? Does it save something?");
 });
 
 app.get("/result-server", (req, res) => {
-  console.log(storage);
   res.send(
     `<h1>Results: </h1> <p>users: </p> ${storage.map((item) =>
       JSON.stringify(item)
